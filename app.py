b1ad30afc763a9100b35af32a805a3822784074b
Nikhil Chadha <nikhilchadha1537@gmail.com>
2024-02-05 15:43:14 +0530

    Added Load_data function

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ import numpy as np
 st.title("Sentiment analysis of tweets about us Airlines")
 st.sidebar.title("Sentiment analysis of tweets about us Airlines")
 
-st.markdown("This application is a Steamlit dashboard to analyze the sentiment of tweets 🐦")
-st.sidebar.markdown("This application is a Steamlit dashboard to analyze the sentiment of tweets 🐦")
+st.markdown("This application is a Streamlit dashboard to analyze the sentiment of tweets 🐦")
+st.sidebar.markdown("This application is a Streamlit dashboard to analyze the sentiment of tweets 🐦")
 
-DATA_URL=("/home/rhyme/Desktop/Project/Tweets.csv")
+DATA_URL=("D:\Study\Third year sem 6\Predictive analysis using statistics\Interactive-Dashboards-Streamlit-Python\Tweets.csv")
+@st.cache_data(persist=True)
 def load_data():
     data=pd.read_csv(DATA_URL)
+    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+    return data
+
+data=load_data()
