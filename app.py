d0f57fa442235f2e29cba877c9962272c2985e5c
Nikhil Chadha <nikhilchadha1537@gmail.com>
2024-02-05 22:49:57 +0530

    Added Choice Plots

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ random_tweet=st.sidebar.radio('Sentiment',('positive','neutral','negative'))
 st.sidebar.markdown(data.query('airline_sentiment== @random_tweet')[["text"]].sample(n=1).iat[0,0])
 
 st.sidebar.markdown("### Number of tweets by sentiment")
-select = st.sidebar.selectbox('Visualization type',['Histogram','Pie Chart'],key='1')
+select = st.sidebar.selectbox('Visualization type',['Histogram','Pie Chart'],key='2')
 sentiment_count=data['airline_sentiment'].value_counts()
 sentiment_count=pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
 
@@ -35,4 +35,25 @@ if not st.sidebar.checkbox("Hide",True):
     else:
         fig=px.pie(sentiment_count,values='Tweets',names='Sentiment')
         st.plotly_chart(fig)
+
+st.sidebar.subheader("When and where are users tweeting from?")
+hour=st.sidebar.slider("Hour of the day",0,23)
+modified_data=data[data['tweet_created'].dt.hour==hour]
+if not st.sidebar.checkbox("Close",True,key='1'):
+    st.markdown("### Tweets locations based on the time of the day")
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
+    st.map(modified_data)
+    if st.sidebar.checkbox("Show raw data", False):
+        st.write(modified_data)
+
+st.sidebar.subheader("Breakdown airline tweets by sentiment")
+choice = st.sidebar.multiselect('Pick airlines',('US Airways','United','American','Southwest','Delta','Virgin America'),key='0')
+
+if len(choice)>0:
+    choice_data=data[data.airline.isin(choice)]
+    fig_choice=px.histogram(choice_data,x='airline' , y='airline_sentiment',histfunc='count', color='airline_sentiment',
+                            facet_col='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600,width=800)
+    st.plotly_chart(fig_choice)
+    
+
 
